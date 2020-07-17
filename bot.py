a00ee879623563455ce119f56873b5babb65892f
oytundd <oytundd@gmail.com>
2020-07-18 00:27:14 +0300

    reply with pics

--- bot.py
+++ bot.py
@@ -1,11 +1,31 @@
 import discord
-import botToken
-theToken = botToken.myToken
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
-    async def on_message(self,message):
-        print('Message from {0.author}: {0.content}'.format(message))
+import logging
+from botToken import myToken
+from discord.ext import commands
 
-client = MyClient(theToken)
-client.run()
+client = discord.Client()
+
+### LOGGER ###
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
+### EVENTS ###
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content == 'man':
+        await message.channel.send(file=discord.File('pictures/man.png'))
+
+    if message.content == 'my man':
+        await message.channel.send(file=discord.File('pictures/myMan.jpg'))
+
+client.run(myToken)
