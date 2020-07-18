3de897157f36c0c624bfd3e8d0b84286b278d965
oytundd <oytundd@gmail.com>
2020-07-18 19:59:52 +0300

    Emote usage
    Counts which emotes are being used, works on multiple servers

--- bot.py
+++ bot.py
@@ -10,7 +10,9 @@ try:
     os.mkdir("pickleJar")
 except:
     pass
-emoteMatch = re.compile('<:.+?:\d+>')
+
+emoteMatch = re.compile("<:.+?:\d+>")
+emoteMatchid = re.compile("<:.+?:(\d+)>")
 bot = commands.Bot(command_prefix='.')
 c = Counter()
 
@@ -44,17 +46,55 @@ async def on_message(message):
         if m:
             serverName = "pickleJar/"+'{0.guild}'.format(message)
             global c
-            c += Counter(m)
-            pickle.dump(c, open(serverName, "wb"))
-            print(c)
+            c = Counter(m)
+            try:
+                with open(serverName,"rb") as toRead:
+                    z = pickle.load(toRead)
+                    x = c + z
+                with open(serverName,"wb") as toWrite:
+                    pickle.dump(x,toWrite)
+            except:
+                with open(serverName,"wb") as toWrite:
+                    pickle.dump(c,toWrite)    
+            """
+            file =  open(serverName, "wb+")
+            try:
+                z = pickle.load(file)
+                x = c + z
+                pickle.dump(x, file)
+                file.close()
+                print(x)
+            except EOFError:
+                pickle.dump(c,file)
+                file.close()
+                print(c)
+            """
+
+
 
     await bot.process_commands(message)
 
 @bot.command()
 async def counter(ctx):
     serverName = "pickleJar/"+str(ctx.message.guild)
-    counterObject = pickle.load(open(serverName,"rb"))
-    await ctx.send("Most popular emotes for the server "+str(ctx.guild)+":\n"+str(counterObject))
+    try:
+        with open(serverName,"rb") as toRead:
+            counterObject = pickle.load(toRead)
+        counterEmbed = discord.Embed(title="Emote usage for "+str(ctx.guild),color =0xE85F5C)
+        counterEmbed.set_thumbnail(url=str(ctx.guild.icon_url))
+        for key in counterObject:
+            counterEmbed.add_field(name=key, value = counterObject[key],inline=True)
+        await ctx.send(embed=counterEmbed)
+            #await ctx.send("Most popular emotes for the server "+str(ctx.guild)+":\n"+str(counterObject))
+    except Exception as e:
+        print(e)
+        #await ctx.send("There are no recorded emotes for this server")
+@bot.command()
+async def icon(ctx):
+    await ctx.send(str(ctx.guild.icon_url))
+@bot.command()
+async def emotes(ctx):
+    print(ctx.guild.emojis)
 
 
 bot.run(myToken)
