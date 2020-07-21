5da05c3451dda1b6745d7986d6ffc4fac1034c0c
oytundd <oytundd@gmail.com>
2020-07-21 22:40:25 +0300

    Added .cocktail,improved on message images,added .up
    .cocktail = post random cocktail recipe
    messages now sent with links instead of uploading it to discord
    .up = uptime

--- bot.py
+++ bot.py
@@ -6,6 +6,9 @@ from collections import Counter
 from botToken import myToken
 from discord.ext import commands
 import os
+import urllib.request, json
+from datetime import datetime
+import humanize
 try:
     os.mkdir("pickleJar")
 except:
@@ -27,6 +30,9 @@ logger.addHandler(handler)
 @bot.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(bot))
+    global launchTime
+    launchTime = datetime.now()
+    
 
 @bot.event
 async def on_message(message):
@@ -34,10 +40,13 @@ async def on_message(message):
         return
 
     if message.content == 'man':
-        await message.channel.send(file=discord.File('pictures/man.png'))
+        #await message.channel.send(file=discord.File('pictures/man.png'))
+        await message.channel.send('https://pbs.twimg.com/media/EUwtJWaXgAE_sZh.png:large')
 
+    if message.content == 'horse':
+        await message.channel.send('https://pbs.twimg.com/media/EUwvxkQXYAA-FY9.jpg:large')
     if message.content == 'my man':
-        await message.channel.send(file=discord.File('pictures/myMan.jpg'))
+        await message.channel.send('https://pbs.twimg.com/media/EazYe-9WoAA0i9L.jpg')
 
     if message.content:
         #print('{0.content} in {0.guild}'.format(message))
@@ -98,5 +107,25 @@ async def emotes(ctx):
 @bot.command()
 async def latency(ctx):
     await ctx.send(bot.latency)
+@bot.command()
+async def cocktail(ctx):
+    with urllib.request.urlopen("https://www.thecocktaildb.com/api/json/v1/1/random.php") as url:
+        cockDict = json.loads(url.read().decode())
+    cockEmbed = discord.Embed(title=cockDict['drinks'][0]['strDrink'],description=cockDict['drinks'][0]['strInstructions'],color =0xE85F5C)
+    cockEmbed.set_thumbnail(url=cockDict['drinks'][0]['strDrinkThumb'])
+    cockEmbed.add_field(name='Glass Type', value= cockDict['drinks'][0]['strGlass'])
+    for i in range(1,16,1):
+        ing = 'strIngredient'+str(i)
+        mes = 'strMeasure'+str(i)
+        if cockDict['drinks'][0][ing] != None:
+            cockEmbed.add_field(name=cockDict['drinks'][0][ing],value = cockDict['drinks'][0][mes])
+
+    await ctx.send(embed=cockEmbed)
+@bot.command()
+async def up(ctx):
+    upCommandTime = datetime.now()
+    upTime =  upCommandTime - launchTime
+    #await ctx.send(upTime.weeks+"week(s)"+str(upTime.days)+"day(s)"+str(upTime.hours)+"hour(s)"+str(upTime.minutes))
+    await ctx.send("Bot has been up for "+ humanize.naturaldelta(upTime))
 
 bot.run(myToken)
