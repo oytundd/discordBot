0a8061131347cecd6c065e51132963f0d2e0aa70
oytundd <oytundd@gmail.com>
2020-07-26 01:43:05 +0300

    Revert "Update botParameters.py"
    This reverts commit 4b609f3185bad2ab065db4c6e4c6e37b866b1d75.

--- botParameters.py
+++ botParameters.py
@@ -1,3 +1,3 @@
 myToken = "NTcyNTkwMzg4NzU4NDQ2MTAz.XxH7YA.5Yl8i51G8Llfqa4rzrYMOQ6wWH0"
 edamameID = "07210b20"
-edamemeToken = "b0a32428795e3122506fae412ebc896f"	
+edamemeToken = "8185c80400f7b6f78dbf7fb1560bae68"	
