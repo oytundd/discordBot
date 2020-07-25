2a689c2783f4129a60411af4c4a998a6d036fd25
oytundd <oytundd@gmail.com>
2020-07-25 16:18:02 +0300

    fixed .counter taking arguments

--- bot.py
+++ bot.py
@@ -89,7 +89,7 @@ async def on_message(message):
     await bot.process_commands(message)
 
 @bot.command()
-async def counter(ctx,arg):
+async def counter(ctx,arg =None):
     serverName = "pickleJar/"+str(ctx.message.guild)
     try:
         with open(serverName,"rb") as toRead:
